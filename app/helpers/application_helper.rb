11607ef4481239f320d90072a6562b427b8c9068
Ryan Yeske <rcyeske@gmail.com>
2011-03-31 17:14:00 -0700

    add gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
 #    doc.sanitize_html = true
     doc.to_html.html_safe
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=20"
+  end
 end
